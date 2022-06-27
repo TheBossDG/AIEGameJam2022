0c2c2376cff24cf816e17fd46e906da65bba3a95
TheBossDG <99941561+TheBossDG@users.noreply.github.com>
2022-06-27 12:12:30 +1000

    Update Enemy Behavior.cs

--- Cerial Killerz - The Rising Spoons Vol 1/Assets/Scenes/Scripts/Enemy Behavior.cs
+++ Cerial Killerz - The Rising Spoons Vol 1/Assets/Scenes/Scripts/Enemy Behavior.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public Vector2 direction = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = direction;
     }
 }
